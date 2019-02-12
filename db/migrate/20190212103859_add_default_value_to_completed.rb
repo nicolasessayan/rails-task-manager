70d8468bad705c2a5e0375651049d9ff02345c8d
Nicolas Essayan <nicolas_essayan@yahoo.fr>
2019-02-12 18:53:52 +0100

    Solved

---
+++ db/migrate/20190212103859_add_default_value_to_completed.rb
@@ -0,0 +1,5 @@
+class AddDefaultValueToCompleted < ActiveRecord::Migration[5.2]
+  def change
+    change_column_default :tasks, :completed, false
+  end
+end
---
+++ db/migrate/20190212105159_change_typeof_details.rb
@@ -0,0 +1,5 @@
+class ChangeTypeofDetails < ActiveRecord::Migration[5.2]
+  def change
+    change_column :tasks, :details, :text
+  end
+end
